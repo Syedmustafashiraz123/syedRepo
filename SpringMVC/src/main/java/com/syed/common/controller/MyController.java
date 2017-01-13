5f00cf678e1fa08a2772a07be86cd28293b9bbe4
Syedmustafashiraz123 <syedmustafa.s@hcl.com>
2017-01-13 21:41:08 +0530

    changes made to class name and added one more controller method and changes in list.jsp

---
+++ SpringMVC/src/main/java/com/syed/common/controller/MyController.java
@@ -0,0 +1,27 @@
+package com.syed.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+
+public class MyController {
+
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public String getGreeting( ModelMap model) {
+
+		model.addAttribute("name", "");
+		return "list";
+	}
+	
+	
+	@RequestMapping(value = "/greeting/{name}", method = RequestMethod.GET)
+	public String getGreeting(@PathVariable String name, ModelMap model) {
+
+		model.addAttribute("name",name);
+		return "list";
+	}
+}
--- SpringMVC/src/main/webapp/WEB-INF/pages/list.jsp
+++ SpringMVC/src/main/webapp/WEB-INF/pages/list.jsp
@@ -1,7 +1,7 @@
 <html>
 <body>
-	<h1>Spring 3 MVC REST web service</h1>
+	<h1>Greeting Page</h1>
 	
-	<h3>Movie Name : ${movie}</h3>	
+	<h3>Hello : ${name}</h3>	
 </body>
 </html>
